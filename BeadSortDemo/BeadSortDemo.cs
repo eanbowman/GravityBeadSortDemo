70228fd2662178e2c4b4dff84ee2b9edb48a0bdf
Ean Bowman <Ean.Bowman@corusent.com>
2017-10-20 17:13:58 -0400

    Initial Commit

---
+++ BeadSortDemo/BeadSortDemo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeadSortDemo
+{
+	public partial class BeadSortDemo : Form
+	{
+		public int sizeToSort = 1000000;
+		public BeadSortDemo()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Console.SetOut(new TextBoxWriter(textBox1));
+			Console.WriteLine("Gravity / Bead Sort Demo");
+			int[] data = RandomList();
+			Console.WriteLine(sizeToSort);
+			//int[] data = { 586, 25, 58964, 8547, 119, 0, 78596 };
+
+			sizeToSort = data.Length - 1;
+			Console.Write("Unsorted beads:");
+			ShowBeads(data);
+			BeadSort(ref data);
+			Console.Write("Sorted beads:");
+			ShowBeads(data);
+			Console.WriteLine("---");
+			Console.WriteLine("Beads are sorted!");
+		}
+
+		private void ShowBeads(int[] data)
+		{
+			for (int i = 0; i <= sizeToSort; i++)
+			{
+				Console.Write(data[i]);
+				Console.Write(" ");
+			}
+			Console.WriteLine();
+		}
+
+		private int[] RandomList()
+		{
+			int[] randomizedList = new int[sizeToSort];
+			int i;
+			Console.WriteLine("Randomizing list of " + sizeToSort + " integers");
+			Random rnd = new Random();
+			for (i = 0; i < sizeToSort; i++)
+			{
+				randomizedList[i] = rnd.Next(0, byte.MaxValue);
+				Console.Write(randomizedList[i]);
+				Console.Write(" ");
+			}
+			Console.WriteLine("");
+			return randomizedList;
+		}
+
+		public static void BeadSort(ref int[] data)
+		{
+			int i, j, max, sum;
+			byte[] beads;
+
+			for (i = 1, max = data[0]; i < data.Length; ++i)
+				if (data[i] > max)
+					max = data[i];
+
+			beads = new byte[max * data.Length];
+
+			for (i = 0; i < data.Length; ++i)
+				for (j = 0; j < data[i]; ++j)
+					beads[i * max + j] = 1;
+
+			for (j = 0; j < max; ++j)
+			{
+				for (sum = i = 0; i < data.Length; ++i)
+				{
+					sum += beads[i * max + j];
+					beads[i * max + j] = 0;
+				}
+
+				for (i = data.Length - sum; i < data.Length; ++i)
+					beads[i * max + j] = 1;
+			}
+
+			for (i = 0; i < data.Length; ++i)
+			{
+				for (j = 0; j < max && Convert.ToBoolean(beads[i * max + j]); ++j) ;
+				data[i] = j;
+			}
+		}
+	}
+}
--- BeadSortDemo/Program.cs
+++ BeadSortDemo/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BeadSortDemo
 {
+	public class TextBoxWriter : TextWriter
+	{
+		TextBox _output = null;
+
+		public TextBoxWriter(TextBox output)
+		{
+			_output = output;
+		}
+
+		public override void Write(char value)
+		{
+			base.Write(value);
+			_output.AppendText(value.ToString());
+		}
+
+		public override Encoding Encoding
+		{
+			get { return System.Text.Encoding.UTF8; }
+		}
+	}
 	static class Program
 	{
 		/// <summary>
@@ -16,7 +38,7 @@ namespace BeadSortDemo
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new BeadSortDemo());
 		}
 	}
 }
